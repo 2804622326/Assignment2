ce24c1eb2bb865df3b445a343db59b3023f7b7b1
Minghao Li <2804622326@qq.com>
2025-03-27 07:35:13 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-import HomeScreen from "./HomeScreen";
-import ResultScreen from "./ResultScreen";
+// App.js (Exercise 3)
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './WelcomeScreen';
+import ProfileScreen from './ProfileScreen';
+import SettingsScreen from './SettingsScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "IS6144 Mock String App" }}
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen 
+          name="Welcome" 
+          component={WelcomeScreen} 
+          options={{
+            title: 'Home Screen',
+            headerStyle: { backgroundColor: '#f4511e' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold' },
+          }}
         />
-        <Stack.Screen
-          name="Result"
-          component={ResultScreen}
-          options={{ title: "Result Screen" }}
+        <Stack.Screen 
+          name="Profile" 
+          component={ProfileScreen} 
+          options={{
+            title: 'Your Profile',
+            headerStyle: { backgroundColor: '#2196F3' },
+            headerTintColor: '#fff',
+          }}
+        />
+        <Stack.Screen 
+          name="Settings" 
+          component={SettingsScreen} 
+          options={{
+            title: 'Settings',
+            // Uncomment the next line to hide the header on this screen:
+            // headerShown: false,
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
